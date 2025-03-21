12c928c6d0c13349048fa579a1ff165faf2df77b
khanhdx-0573 <dang.xuan.khanh@sun-asterisk.com>
2025-03-21 10:57:40 +0700

    Feature #85837: add product to cart

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "custom/main", preload: true
+pin "custom/add_to_cart", preload: true
 pin "@fontawesome/fontawesome-free", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"
