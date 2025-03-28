27bb944828c2e18adaae1b4965f1be40f1c88698
khanhdx-0573 <dang.xuan.khanh@sun-asterisk.com>
2025-03-28 16:54:06 +0700

    Feature #85844: implement product filtering and search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,8 @@ class ProductsController < ApplicationController
 
   def index
     @categories = Category.all
-    products = Product.by_category(params[:category_id])
+    products = Product.by_category params[:category_id]
+    products = products.by_name(params[:name]).by_sort(params[:sort])
     @pagy, @products = pagy products, limit: Settings.pagy_items
   end
   private
